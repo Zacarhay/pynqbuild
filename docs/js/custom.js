07d69eb9bfcf3fc4510c2527c010c79715c2c192
Zacarhay <zacharia@xilinx.com>
2022-09-07 10:40:01 +0100

    Revert "1"
    This reverts commit 6728a63e2966c2283c010a694a1677ae95a5ef11.

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,25 +1,7 @@
 /*************************************************************************
  * Your custom JS file
  *************************************************************************/
- const buttons = document.querySelectorAll('.project');
- const overlay = document.querySelector('.overlay');
- const overlayImage = document.querySelector('.overlay__inner img');
- 
- function open(e) {
-   overlay.classList.add('open');
-   const src= e.currentTarget.querySelector('img').src;
-   overlayImage.src = src;
- }
- 
- function close() {
-   overlay.classList.remove('open');
- }
- 
- buttons.forEach(button => button.addEventListener('click', open));
- overlay.addEventListener('click', close);
 
-
-/*********************************/
 (function () {
   "use strict";
 
